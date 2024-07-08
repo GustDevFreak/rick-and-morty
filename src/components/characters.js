56551370ce0b4daace6ccb17a2706af8726fa4a3
Gustavo <kg.2k.pl@gmail.com>
2024-07-08 10:08:08 -0400

    iterating the data that comes from the API with the map() function

--- src/components/characters.js
+++ src/components/characters.js
@@ -1,8 +1,18 @@
 export default function Characters(props) {
-    console.log(props);
+    // extraccion de datos mediante un array structuring
+    const {characters} = props;
   return (
-    <div>
-        <h1>Component Characters JS</h1>
+    <div className="characters">
+        <h1>Personajes</h1>
+        <span className="back-home">Volver a la Home</span>
+        <div className="container-characters">
+            {/* Recorriendo los datos que vienen de la API */}
+            {characters.map((character, index) => ( // Return Implicito
+                <div className="character-container" key={index}>
+                    <p>{character.name}</p>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
