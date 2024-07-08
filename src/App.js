64f8da10062ad5bad7c47ac60f1d137f0a0f6002
Gustavo <kg.2k.pl@gmail.com>
2024-07-08 09:40:28 -0400

    rendering two views using a conditional, home page and characters, and sending data from the api to the characters component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import imgRickMorty from './img/rick-morty.png';
 import './App.css';
 import { useState } from 'react';
+import Characters from './components/characters';
 
 function App() {  
   const [characters, setCharacters] = useState(null);
@@ -11,14 +12,21 @@ function App() {
     setCharacters(characterApi.results);
   }
 
-  console.log(characters);
-
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick & Morty</h1>
-        <img src={imgRickMorty} className='img-home' alt='Rick & Morty' ></img>
-        <button onClick={reqApi} className='btn-search'>Search Characters</button>
+        {/* 1. Si hay characters renderiza el componente Characters, caso contrario el JSX */}
+        {characters ? (
+          <Characters characters = {characters}/>
+        ) : (
+          // 2. Encapsulando este JSX en un fragment para que pueda ser renderizado
+          <>
+          <img src={imgRickMorty} className='img-home' alt='Rick & Morty' ></img>
+          <button onClick={reqApi} className='btn-search'>Search Characters</button>
+          </>
+        )}
+        
       </header>
     </div>
   );
